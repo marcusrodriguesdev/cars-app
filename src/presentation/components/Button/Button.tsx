9a4b0b5fd7d464129a80566bfeeb80a349dc84a2
Marcus Rodrigues <marcus.rodrigues@rethink.dev>
2023-07-12 11:43:04 -0300

    feat: add jsdocs from button component

--- src/presentation/components/Button/Button.tsx
+++ src/presentation/components/Button/Button.tsx
@@ -1,25 +1,32 @@
-import React from 'react'
+import React from 'react';
 
-import { ButtonStyled, Text } from './styles'
+import { ButtonStyled, Text } from './styles';
 
 export interface ButtonProps {
-  label: string
-  disabled?: boolean
-  onClick: () => void
+  label: string;
+  disabled?: boolean;
+  onClick: () => void;
 }
 
+/**
+ * A stylized button for the application
+ * @param label     The label of the button
+ * @param disabled  The boolean to disable the button
+ * @param onClick   The function to be called when the button is pressed
+ */
+
 const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
-  const { label, disabled, onClick } = props
+  const { label, disabled, onClick } = props;
 
   const handlePress = () => {
-    if (!disabled) onClick()
-  }
+    if (!disabled) onClick();
+  };
 
   return (
     <ButtonStyled onPress={handlePress} disabled={disabled} isDisabled={disabled}>
       <Text>{label}</Text>
     </ButtonStyled>
-  )
-}
+  );
+};
 
-export { Button }
+export { Button };
