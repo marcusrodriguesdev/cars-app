3ed47c249caeec7a44d52866d79dfa783b26ffdf
Marcus Rodrigues <marcus.rodrigues@rethink.dev>
2023-07-12 11:40:50 -0300

    refactor: move components and assests to presentation

--- src/presentation/screens/SignIn/SignIn.tsx
+++ src/presentation/screens/SignIn/SignIn.tsx
@@ -1,12 +1,59 @@
 import React from 'react';
-import { StatusBar, Text, View } from 'react-native';
-import { Container } from './styles';
+import { Keyboard, StatusBar } from 'react-native';
+import { ButtonContainer, Container, ImageStyled, InputContainer, Subtitle, Title } from './styles';
+import { useForm, Controller } from 'react-hook-form';
+import { Button, Input } from '../../components';
+import { SignInFormData } from '../../types/signIn';
+
+const logo = require('../../assets/carHome.png');
 
 const SignIn: React.FC = () => {
+  const { control, handleSubmit, formState } = useForm<SignInFormData>();
+
+  const onSubmit = (data: SignInFormData) => {
+    // Processar os dados do formulário
+    console.log(data);
+  };
+
   return (
-    <Container>
-      <StatusBar barStyle="dark-content" />
-      <Text>SignIn</Text>
+    <Container onPress={() => Keyboard.dismiss()}>
+      <StatusBar barStyle="light-content" />
+      <ImageStyled source={logo} />
+      <Title>Bem-vindo ao CarList</Title>
+      <Subtitle>Explore seu mundo automotivo!</Subtitle>
+      <InputContainer>
+        <Controller
+          control={control}
+          name="username"
+          defaultValue=""
+          render={({ field: { onChange, value } }) => (
+            <Input
+              placeholder="Escreva seu nome de usuário"
+              value={value}
+              onChangeText={onChange}
+              secureText={false}
+            />
+          )}
+          rules={{ required: true }}
+        />
+        <Controller
+          control={control}
+          name="password"
+          defaultValue=""
+          render={({ field: { onChange, value } }) => (
+            <Input
+              placeholder="Escreva sua senha"
+              value={value}
+              onChangeText={onChange}
+              secureText
+            />
+          )}
+          rules={{ required: true }}
+        />
+      </InputContainer>
+      <ButtonContainer>
+        <Button label="Entrar" onClick={handleSubmit(onSubmit)} disabled={!formState.isValid} />
+      </ButtonContainer>
     </Container>
   );
 };
--- src/presentation/screens/SignIn/styles.ts
+++ src/presentation/screens/SignIn/styles.ts
@@ -1,8 +1,36 @@
 import { styled } from 'styled-components/native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
-export const Container = styled(SafeAreaView)`
+export const Container = styled.Pressable`
   flex: 1;
-  justify-content: center;
-  align-items: center;
+  background-color: #2c2b34;
+`;
+
+export const ImageStyled = styled.Image`
+  width: 200;
+  height: 400;
+  margin-top: 10;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 20px;
+  font-weight: 700;
+  margin-left: 24px;
+`;
+
+export const Subtitle = styled.Text`
+  color: #8e8e8e;
+  font-size: 18px;
+  font-weight: 400;
+  margin-left: 24px;
+  margin-bottom: 10;
+`;
+
+export const InputContainer = styled.View`
+  padding-horizontal: 24px;
+`;
+
+export const ButtonContainer = styled.View`
+  margin-top: 56px;
+  padding-horizontal: 24px;
 `;
